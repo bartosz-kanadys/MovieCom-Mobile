7f75a27b736e51d0c728ceda04dd982f0abbf9b1
Bartosz Kanadys <kanadysbartosz@gmail.com>
2025-04-09 17:56:12 +0200

    Add movie list item ui

---
+++ app/src/main/java/com/moviecommobile/presentation/screens/movie_list_screen/composables/MovieListItem.kt
@@ -0,0 +1,177 @@
+package com.moviecommobile.presentation.screens.movie_list_screen.composables
+
+import android.util.Log
+import com.moviecommobile.R
+import androidx.compose.runtime.*
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.compose.AppTheme
+import com.moviecommobile.domain.Movie
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextOverflow
+import coil3.compose.rememberAsyncImagePainter
+import kotlin.math.round
+
+
+@Composable
+fun MovieListItem(
+    movie: Movie,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Surface(
+        shape = RoundedCornerShape(25.dp),
+        color = MaterialTheme.colorScheme.primaryContainer
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+                .height(IntrinsicSize.Min),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(100.dp),
+                contentAlignment = Alignment.Center
+            ) {
+
+                var imageLoadResult by remember {
+                    mutableStateOf<Result<Painter>?>(null)
+                }
+                val painter = rememberAsyncImagePainter(
+                    model = movie.poster,
+                    onSuccess = {
+                        imageLoadResult = if (it.painter.intrinsicSize.width > 1 && it.painter.intrinsicSize.height >1) {
+                            Result.success(it.painter)
+                        } else {
+
+                            Result.failure(Exception("Invalid image size"))
+
+                        }
+                    },
+                    onError = {
+                        it.result.throwable.printStackTrace()
+                        imageLoadResult = Result.failure(it.result.throwable)
+
+                    }
+                )
+
+                when (val result = imageLoadResult) {
+                    null -> CircularProgressIndicator()
+                    else -> {
+                        Image(
+                            painter = if(result.isSuccess) painter else painterResource(id = R.drawable.image_not_found),
+                            contentDescription = movie.title,
+                            contentScale = if (result.isSuccess) {
+                                ContentScale.Crop
+                            } else {
+                                ContentScale.Fit
+                            },
+                            modifier = Modifier.aspectRatio(
+                                ratio = 0.65f,
+                                matchHeightConstraintsFirst = true
+                            )
+                        )
+                    }
+                }
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = movie.title,
+                    style = MaterialTheme.typography.titleLarge,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                movie.genres?.firstOrNull().let { authorName ->
+                    Text(
+                        text = ("$authorName, "),
+                        style = MaterialTheme.typography.bodyLarge,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
+                movie.imdb?.rating?.let { rating ->
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "${round(rating * 10) / 10}",
+                            style = MaterialTheme.typography.bodyLarge,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                        Icon(
+                            imageVector = Icons.Default.Star,
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+
+                }
+            }
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.primary,
+                modifier = Modifier
+                    .size(36.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MovieListItemPreview(){
+    val movie = Movie(
+        title = "Movie",
+        year = 2024,
+        genres = listOf("Action", "Drama"),
+        plot = "This is a movie plot",
+        poster = "https://m.media-amazon.com/images/M/MV5BMjNlMThmNzItMTZlMS00YzJkLTk1MzktYzIyMzllOGFmZmRlXkEyXkFqcGdeQXVyMzE0MjY5ODA@._V1_SY1000_SX677_AL_.jpg",
+        released = null,
+        awards = null,
+        type = null,
+        imdb = Movie.Imdb(8.8)
+    )
+    AppTheme {
+        MovieListItem(movie, {})
+    }
+
+}
